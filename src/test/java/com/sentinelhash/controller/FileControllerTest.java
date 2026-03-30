36f288a10cb73dbd324023d2ebac7372496a04ca
Kyle <KileHarrington@gmail.com>
2026-03-30 18:28:41 -0400

    Fix test: import SecurityConfig to apply permitAll on /verify endpoint

--- src/test/java/com/sentinelhash/controller/FileControllerTest.java
+++ src/test/java/com/sentinelhash/controller/FileControllerTest.java
@@ -8,7 +8,6 @@ import com.sentinelhash.service.IntegrityService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -28,7 +27,7 @@ import static org.springframework.security.test.web.servlet.request.SecurityMock
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest(value = FileController.class, excludeAutoConfiguration = SecurityAutoConfiguration.class)
+@WebMvcTest(FileController.class)
 @Import(SecurityConfig.class)
 class FileControllerTest {
 
