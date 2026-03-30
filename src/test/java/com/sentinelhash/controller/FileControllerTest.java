c1ef78589551b78e681122b1380c9ee0f1b96005
Kyle <KileHarrington@gmail.com>
2026-03-30 18:23:36 -0400

    Exclude SecurityAutoConfiguration to use real SecurityConfig in tests

--- src/test/java/com/sentinelhash/controller/FileControllerTest.java
+++ src/test/java/com/sentinelhash/controller/FileControllerTest.java
@@ -8,6 +8,7 @@ import com.sentinelhash.service.IntegrityService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -27,7 +28,7 @@ import static org.springframework.security.test.web.servlet.request.SecurityMock
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest(FileController.class)
+@WebMvcTest(value = FileController.class, excludeAutoConfiguration = SecurityAutoConfiguration.class)
 @Import(SecurityConfig.class)
 class FileControllerTest {
 
